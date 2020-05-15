ba0143c47b0038d797e86417e0dec4bd0b0ccdb7
KatsunoriGunji <l.hvokj@gmail.com>
2020-05-15 15:52:30 +0900

    update Cui.java

--- src/Sample/cui.java
+++ src/Sample/cui.java
@@ -4,15 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class cui {
+public class Cui {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(
 				new InputStreamReader(System.in)
 			);
-			System.out.println("Please input your name");
+			System.out.println("数式を入力して下さい");
 			String line = reader.readLine();
-			System.out.println("Hello " + line + "!");
 	}
 
 }
