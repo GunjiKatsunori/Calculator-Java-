1017a15a0b28037354a99759050bd429ef6e40d0
KatsunoriGunji <l.hvokj@gmail.com>
2020-05-15 15:22:43 +0900

    define Node class

---
+++ src/Sample/Node.java
@@ -0,0 +1,19 @@
+package Sample;
+
+class Node<T>{
+	private T value;
+	private Node<T> left;
+	private Node<T> right;
+	
+	public Node(T value){
+		this.value = value;
+	}
+
+	public void setLeft(Node<T> node){
+		this.left = node;
+	}
+
+	public void setRight(Node<T> node){
+		this.right = node;
+	}
+}
