09933452206f88ec7a4b2ed468b0cc18f40409bb
GunjiKatsunori <65153979+GunjiKatsunori@users.noreply.github.com>
2020-05-20 16:05:19 +0900

    Create Token.java

---
+++ src/Token.java
@@ -0,0 +1,39 @@
+public class Token {
+	private String token;
+	private String category;
+
+	public Token(String token, String category) {
+		this.token = token;
+		this.category = category;
+	}
+
+	public void append(char ch) {
+		token += ch;
+	}
+
+	public static String detCategory(char ch) {
+		String category = "";
+
+		if ((ch >= '0' && ch <= '9') || ch == '.') {
+			category = "digit";
+		}
+		else if (ch == '+' || ch == '-') {
+			category = "+-";
+		}
+		else if (ch == '*' || ch == '/') {
+			category = "*/";
+		}
+		else if (ch == '(') {
+			category = "(";
+		}
+		else if (ch == ')') {
+			category = ")";
+		}
+		else if (ch == ' ') {
+			category = "space";
+		}
+		else {
+			category = "character";
+		}
+	}
+}
