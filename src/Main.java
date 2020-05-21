a59e9d786ef329177da52b8664e9981e2bdac5fd
KatsunoriGunji <l.hvokj@gmail.com>
2020-05-21 18:46:57 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import java.io.BufferReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -11,43 +12,44 @@ public class Main {
 		String formulaStr = stdin.readLine();
 		tokenize(formulaStr);
 	}
-}
 
-/* 数式をトークンに分解するメソッド
- * 引数：in String 入力された数式
- * 戻り値：分割された字句のリスト */
-public static List<Token> tokenize(String formulaStr) {
-	List<Token> tokenList = new ArrayList<Token>();
-	Token token = new Token("", "space");
+	/* 数式をトークンに分解するメソッド
+	* 引数：in String 入力された数式
+	* 戻り値：分割された字句のリスト */
+	public static List<Token> tokenize(String formulaStr) {
+		List<Token> tokenList = new ArrayList<Token>();
+		Token token = new Token("", "space");
 
-	while (int i = 0; i < formulaStr.length(); i++) {
-		ch = formulaStr.charAt(i);
-		category = detCategory(ch);
-		if (token.category.equals(category)) { //chがtokenに続く文字の場合
-			token.append(ch);
-		}
-		else {
-			if (token.category != "space"){// tokenが空白でない場合
-				tokenList.add(token)
+		for (int i = 0; i < formulaStr.length(); i++) {
+			char ch = formulaStr.charAt(i);
+			String category = Token.detCategory(ch);
+			if (token.getCategory().equals(category)) { //chがtokenに続く文字の場合
+				token.append(ch);
 			}
-			token.set("", "space");
-			if (ch.category != "space"){ // chが空白でない場合
-				String newToken = "" + ch;
-				token.set(newToken, category);
+			else {
+				if (!token.getCategory().equals("space")){// tokenが空白でない場合
+					tokenList.add(token);
+				}
+				token.set("", "space");
+				if (!category.equals("space")){ // chが空白でない場合
+					String newToken = "" + ch;
+					token.set(newToken, category);
+				}
 			}
 		}
+		return tokenList;
 	}
-	return tokenList;
-}
 
-public static List<Node> composeTree(List<Token> tokenList) {
-	List<Node> nodeList    = new ArrayList<Node>();
-	List<Node> nodePointer = new ArrayList<Node>();
+	public static List<Node> composeTree(List<Token> tokenList) {
+		List<Node> nodeList    = new ArrayList<Node>();
+		List<Node> nodePointer = new ArrayList<Node>();
 
-	Token token = new Token();
+		Token token = new Token("", "space");
 
-	for (i=0; i<tokenList.size(); i++) {
+		for (int i=0; i<tokenList.size(); i++) {
 
+		}
+		return nodeList;
 	}
-	return nodeList;
+
 }
