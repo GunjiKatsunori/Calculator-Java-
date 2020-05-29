c47c5f6796a58bfe501eb7ed14b135bb106937b7
KatsunoriGunji <l.hvokj@gmail.com>
2020-05-29 10:53:15 +0900

    Update Node.java

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -25,17 +25,39 @@ public class Node extends Token {
 
 	public static List<Node> addTree(List<Node> nodeList, Token token) {
 		Node newNode = new Node(token);
-		if (nodeList.size() >= 1) {
-			currentNode = nodeList.get(nodeList.size() - 1);
+
+		// currentNodeの更新
+		if (nodeList.size() > 0) {
+			if (nodeList.get(nodeList.size() - 1).get().equals(")")) {
+				currentNode = lastOperator;
+			}
+			else {
+				currentNode = nodeList.get(nodeList.size() - 1);
+			}
 		}
 
-		// 演算子の場合、優先度にしたがってノードの位置を決める
-		if (newNode.isOperator() || newNode.getCategory().equals(")") ) {
+		// 優先度にしたがってノードの位置を決める
+		if (newNode.isOperator()) {
+			// 演算子の場合、結合優先度にしたがって移動
 			while ( currentNode.parent != null &&
 					currentNode.parent.isPrefThan(newNode) ) {
 				currentNode = currentNode.parent;
 			}
+			// 「(」を超えないように１つ上の親ノードに移動する
+			if (currentNode.parent != null &&
+				!(currentNode.parent.get().equals("("))
+				) {
+				currentNode = currentNode.parent;
+			}
 		}
+		else if (newNode.getCategory().equals(")")) {
+			// 「)」の場合、「(」の位置に移動する
+			while ( !currentNode.parent.get().equals("(") ) {
+				currentNode = currentNode.parent;
+			}
+			currentNode = currentNode.parent;
+		}
+
 		// ノードの結合
 		return connect(nodeList, newNode);
 	}
@@ -59,7 +81,7 @@ public class Node extends Token {
 			return 2;
 		}
 		else if (opr.equals("(") || opr.equals(")")) {
-			return 100;
+			return 0;
 		}
 		else {
 			return 0;
@@ -70,21 +92,21 @@ public class Node extends Token {
 		if ( newNode.isOperator() ) { // 演算子の場合
 			// 挿入位置の検索
 			int i = searchNode(nodeList, currentNode);
-			List<Node> list_a = new ArrayList<Node>();
-			List<Node> list_b = new ArrayList<Node>();
+			List<Node> latter = new ArrayList<Node>();
+			List<Node> former = new ArrayList<Node>();
 			if (i >= 0) {
-				list_a = nodeList.subList(i, nodeList.size());
+				latter = nodeList.subList(i, nodeList.size());
 			}
 			if (i >= 1) {
-				list_b = nodeList.subList(0, i);
+				former = nodeList.subList(0, i);
 			}
 
 			// newNodeの挿入
 			List<Node> tmpList = new ArrayList<Node>();
 			tmpList.add(newNode);
-			tmpList.addAll(list_a);
-			list_b.addAll(tmpList);
-			nodeList = list_b;
+			tmpList.addAll(latter);
+			former.addAll(tmpList);
+			nodeList = former;
 
 			// ノードの親子関係
 			newNode.setParent(currentNode.getParent());
@@ -94,33 +116,44 @@ public class Node extends Token {
 			lastOperator = newNode;
 		}
 		else if ( newNode.getCategory().equals(")") ) { // )の場合
-			// 挿入位置の検索
-			int i = searchNode(nodeList, currentNode);
-			List<Node> list_a = nodeList.subList(i+1, nodeList.size());
-			List<Node> list_b = new ArrayList<Node>();
-			if (i >= 1) {
-				list_b = nodeList.subList(0, i);
-			}
-
-			// newNodeの挿入
-			list_b.addAll(list_a);
-			nodeList = list_b;
+			// newNodeの追加
+			nodeList.add(newNode);
 
 			// 構文木の親子関係
-			nodeList.get(i+1).setParent(nodeList.get(i-1));
+			int i = searchNode(nodeList, currentNode);
+			newNode.setParent(nodeList.get(i));
+
+			// 最後に追加された演算子の更新
+			lastOperator = nodeList.get(i);
+		}
+		else if ( newNode.getCategory().equals("(") ) { // (の場合
+			// newNodeの追加
+			nodeList.add(newNode);
+
+			// 親子関係の設定
+			if (lastOperator.get() != "") {
+				// 最初に構文木に(を入れるとき、親ノードに仮置きのlastOperatが入らないようにする
+				newNode.setParent(lastOperator);
+			}
+
+			// 最後に追加された演算子として登録
+			lastOperator = newNode;
 		}
 		else { // 数字の場合
 			// newNodeの追加
 			nodeList.add(newNode);
 
 			// 親子関係の設定
-			newNode.setParent(lastOperator);
+			if (lastOperator.get() != "") {
+				// 最初に構文木に数字を入れるとき、親ノードに仮置きのlastOperatが入らないようにする
+				newNode.setParent(lastOperator);
+			}
 		}
 		return nodeList;
 	}
 
 	private static int searchNode(List<Node> nodeList, Node node) {
-		if (nodeList.size() == 0) {
+		if (nodeList.size() == 0) { // 最初の操作ではi=0
 			return 0;
 		}
 		for (int i=0; i<nodeList.size(); i++) {
